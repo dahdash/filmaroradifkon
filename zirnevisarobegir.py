0514cb7eb42796d8006e1df84241db979f0414bd
K1 Reshadi <k1.reshadi@gmail.com>
2022-09-08 12:44:12 +0430

    Update zirnevasrobegir to get subs from OpenSubtitles

--- zirnevisarobegir.py
+++ zirnevisarobegir.py
@@ -1,11 +1,16 @@
 from babelfish import Language
 from subliminal import download_best_subtitles, region, save_subtitles, scan_videos
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 region.configure('dogpile.cache.dbm', arguments={'filename': 'cachefile.dbm'})
 videos = scan_videos(os.getcwd())
-subtitles = download_best_subtitles(videos, {Language('eng'), Language('fas')})
+subtitles = download_best_subtitles(videos, {Language('eng'), Language('fas')},
+        providers=['opensubtitles'], provider_configs={'opensubtitles':
+            {'username': os.getenv('OPENSUBTITLES_USERNAME'), 'password': os.getenv('OPENSUBTITLES_PASSWORD')}})
 for v in videos:
     print(v)
+    print(subtitles[v])
     save_subtitles(v, subtitles[v])
 
